d76bb20b8c51fded0393c3b3f4594c9d2cdbda1d
Thanh Le <99728087+LeNganThanh@users.noreply.github.com>
2022-08-14 21:54:18 +0200

    giving condition for the last input box

--- script.js
+++ script.js
@@ -2,7 +2,21 @@
 const codeContainer = document.querySelector(".code-container");
 let input = document.querySelectorAll(".code");
 input.forEach(val => {
-  val.addEventListener("keyup", function () {
-    val.nextElementSibling.focus();
+  val.addEventListener("keyup", function (e) {
+    if (!val.nextElementSibling) {
+      val.focus();
+    } else {
+      val.nextElementSibling.focus();
+    }
   });
 });
+
+// for (let i = 0; i < input.length; i++) {
+//   input[i].addEventListener("keyup", function () {
+//     if (i !== input.length - 1) {
+//       input[i + 1].focus();
+//     } else if (i === input.length - 1) {
+//       input[i].focus();
+//     }
+//   });
+// }
