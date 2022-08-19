a38c48135400299a586bc0a84b4515a360565c8c
Thanh Le <99728087+LeNganThanh@users.noreply.github.com>
2022-08-20 00:05:31 +0200

    adding some condition for input box

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
 "use strict";
-const codeContainer = document.querySelector(".code-container");
-let input = document.querySelectorAll(".code");
+
+/* const input = document.querySelectorAll(".code");
+input[0].focus();
 input.forEach(val => {
   val.addEventListener("keyup", function (e) {
-    if (!val.nextElementSibling) {
-      val.focus();
+    if (e.key >= 0 && e.key <= 9) {
+      !val.nextElementSibling
+        ? input[0].focus()
+        : val.nextElementSibling.focus();
     } else {
-      val.nextElementSibling.focus();
+      alert("Accept only number");
     }
   });
 });
+ */
+/* another solution */
+const input = document.querySelectorAll(".code");
+input[0].focus();
+for (let i = 0; i < input.length; i++) {
+  input[i].addEventListener("keyup", function (e) {
+    if (e.key >= 0 && e.key <= 9) {
+      i !== input.length - 1 ? input[i + 1].focus() : input[0].focus();
+    } else {
+      alert("Accept only number");
+    }
+  });
+}
 
-// for (let i = 0; i < input.length; i++) {
-//   input[i].addEventListener("keyup", function () {
-//     if (i !== input.length - 1) {
-//       input[i + 1].focus();
-//     } else if (i === input.length - 1) {
-//       input[i].focus();
-//     }
-//   });
-// }
+//solution from teacher------------------
+/* 
+const boxes = document.querySelectorAll(".code");
+
+boxes[0].focus();
+
+boxes.forEach((item, idx) => {
+  item.addEventListener("keydown", e => {
+    if (e.key >= 0 && e.key <= 9) {
+      boxes[idx].value = "";
+
+      setTimeout(() => boxes[idx >= 5 ? 0 : idx + 1].focus(), 100);
+    } else {
+      alert("Yo only numbers allowed");
+    }
+  });
+});
+ */
